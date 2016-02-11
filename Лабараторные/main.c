b8cd658c153b270b7a16a320cac19bd542f4e6da
Lagunov <lagunovvlad1@gmail.com>
2016-02-11 11:15:06 +0300

    Initial Commit

---
+++ Лабараторные/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Лабараторные
+//
+//  Created by Владислав Лагунов on 11.02.16.
+//  Copyright © 2016 Владислав Лагунов. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
